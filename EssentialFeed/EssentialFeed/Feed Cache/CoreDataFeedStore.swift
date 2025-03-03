3757fbeb7c1a44eb3c993736635e406a1f1b46a9
Yuriy Yasinskyy <yura.yasinskyy@gmail.com>
2025-03-03 07:20:12 +0100

    Change order of methods in FeedStore protocol to better represent the order of operations we usually perform

---
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Work on 03.03.2025.
+//
+
+import Foundation
--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -14,19 +14,19 @@ public enum RetrieveCachedFeedResult {
 }
 
 public protocol FeedStore {
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
     typealias RetrievalCompletion = (RetrieveCachedFeedResult) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    typealias DeletionCompletion = (Error?) -> Void
     
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads if needed.
-    func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    
+    func retrieve(completion: @escaping RetrievalCompletion)
+
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads if needed.
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
     
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads if needed.
-    func retrieve(completion: @escaping RetrievalCompletion)
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
 }
