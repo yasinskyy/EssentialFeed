5f46e6979e67670e95c5149b55302765cb2d54d8
Yuriy Yasinskyy <yura.yasinskyy@gmail.com>
2025-03-03 07:15:26 +0100

    Comment integration tests

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -9,5 +9,18 @@ import XCTest
 import EssentialFeed
 
 final class EssentialFeedCacheIntegrationTests: XCTestCase {
-
+//    func test_load_deliversNoItemsOnEmptyCache() {
+//        let sut = makeSUT()
+//
+//        let exp = expectation(description: "Wait for load completion")
+//        sut.load { result in
+//            switch result {
+//            case let .success(imageFeed):
+//                XCTAssertEqual(imageFeed, [], "Expected empty feed")
+//            case let .failure(error):
+//                XCTFail("Expected successful feed result, got \(error) instead")
+//            }
+//        }
+//        wait(for: [exp], timeout: 1.0)
+//    }
 }
