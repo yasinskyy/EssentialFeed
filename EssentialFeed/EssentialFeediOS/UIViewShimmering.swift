88490c5392db67f25cc4296143407d2ebf11027c
Yuriy Yasinskyy <yura.yasinskyy@gmail.com>
2025-03-20 13:59:47 +0100

    Feed image view loading indicator is visible while loading image

---
+++ EssentialFeed/EssentialFeediOS/UIViewShimmering.swift
@@ -0,0 +1,60 @@
+//
+//  UIViewShimmering.swift
+//  EssentialFeediOS
+//
+//  Created by Work on 20.03.2025.
+//
+
+import UIKit
+
+extension UIView {
+    public var isShimmering: Bool {
+         set {
+             if newValue {
+                 startShimmering()
+             } else {
+                 stopShimmering()
+             }
+         }
+         get {
+             layer.mask is ShimmeringLayer
+         }
+    }
+    
+    public func startShimmering() {
+        layer.mask = ShimmeringLayer(size: bounds.size)
+    }
+    
+    public func stopShimmering() {
+        layer.mask = nil
+    }
+    
+    private class ShimmeringLayer: CAGradientLayer {
+        private static var shimmerAnimationKey: String { return "shimmer" }
+        private var observer: Any?
+        
+        convenience init(size: CGSize) {
+            self.init()
+            
+            let white = UIColor.white.cgColor
+            let alpha = UIColor.white.withAlphaComponent(0.75).cgColor
+            
+            colors = [alpha, white, alpha]
+            startPoint = CGPoint(x: 0.0, y: 0.4)
+            endPoint = CGPoint(x: 1.0, y: 0.6)
+            locations = [0.4, 0.5, 0.6]
+            frame = CGRect(x: -size.width, y: 0, width: size.width*3, height: size.height)
+            
+            let animation = CABasicAnimation(keyPath: #keyPath(CAGradientLayer.locations))
+            animation.fromValue = [0.0, 0.1, 0.2]
+            animation.toValue = [0.8, 0.9, 1.0]
+            animation.duration = 1.25
+            animation.repeatCount = .infinity
+            add(animation, forKey: ShimmeringLayer.shimmerAnimationKey)
+            
+            observer = NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: nil) { [weak self] _ in
+                self?.add(animation, forKey: ShimmeringLayer.shimmerAnimationKey)
+            }
+        }
+    }
+}
--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -126,6 +126,27 @@ final class FeedViewControllerTests: XCTestCase {
         
     }
     
+    func test_feedImageViewLoadingIndicator_isVisibleWhileLoadingImage() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        sut.simulateAppearance()
+        loader.completeFeedLoading(with: [makeImage(), makeImage()])
+        
+        let view0 = sut.simulateFeedImageViewVisible(at: 0)
+        let view1 = sut.simulateFeedImageViewVisible(at: 1)
+        XCTAssertEqual(view0?.isShowingImageLoadingIndicator, true, "Expected loading indicator for first image view while loading first image")
+        XCTAssertEqual(view1?.isShowingImageLoadingIndicator, true, "Expected loading indicator for second image view while loading second image")
+        
+        loader.completeImageLoading(at: 0)
+        XCTAssertEqual(view0?.isShowingImageLoadingIndicator, false, "Expected no loading indicator for first image view once first image loading completes successfully")
+        XCTAssertEqual(view1?.isShowingImageLoadingIndicator, true, "Expected no loading indicator state change for second image view once first image loading completes successfully")
+        
+        loader.completeImageLoadingWithError(at: 1)
+        XCTAssertEqual(view0?.isShowingImageLoadingIndicator, false, "Expected no loading indicator state change for first image view once second image loading completes with error")
+        XCTAssertEqual(view1?.isShowingImageLoadingIndicator, false, "Expected no loading indicator for second image view once second image loading completes with error")
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
@@ -194,14 +215,28 @@ final class FeedViewControllerTests: XCTestCase {
             }
         }
         
-        private(set) var loadedImageURLs = [URL]()
+        private var imageRequests = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        var loadedImageURLs: [URL] {
+            return imageRequests.map { $0.url }
+        }
+        
         private(set) var cancelledImageURLs = [URL]()
         
-        func loadImageData(from url: URL) -> FeedImageDataLoaderTask {
-            loadedImageURLs.append(url)
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+            imageRequests.append((url, completion))
             return TaskSpy { [weak self] in
                 self?.cancelledImageURLs.append(url)
             }
+        }
+        
+        func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
+            imageRequests[index].completion(.success(imageData))
+        }
+        
+        func completeImageLoadingWithError(at index: Int = 0) {
+            let error = NSError(domain: "an error", code: 0)
+            imageRequests[index].completion(.failure(error))
         }
     }
 }
@@ -272,6 +307,7 @@ private extension FeedImageCell {
     var isShowingLocation: Bool { return !locationContainer.isHidden }
     var locationText: String? { return locationLabel.text }
     var descriptionText: String? { return descriptionLabel.text }
+    var isShowingImageLoadingIndicator: Bool { return feedImageContainer.isShimmering }
 }
 
 private extension UIRefreshControl {
