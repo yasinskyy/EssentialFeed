071265f75ecfcd959462e83142509acb9dabf29c
Yuriy Yasinskyy <yurii.yasinskyy@innocode.no>
2025-03-24 11:23:40 +0100

    Group feed UI production and test files in MVC folders structure to clarift there's separation

---
+++ EssentialFeed/EssentialFeediOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Work on 24.03.2025.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
