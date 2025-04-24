ebc5884f5182043428d234a927f8e8944db2e7e5
Yuriy Yasinskyy <yurii.yasinskyy@innocode.no>
2025-04-24 11:20:33 +0200

    Move `FeedImageViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  FeedImageViewModel.swift
+//  EssentialFeed
+//
+//  Created by Work on 24.04.2025.
+//
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
