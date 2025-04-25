9bbcc660ec14bb931f7c8f0ff4fa43479a18da58
Yuriy Yasinskyy <yura.yasinskyy@gmail.com>
2025-04-25 16:02:04 +0200

    `LocalFeedImageDataLoader.saveImageDataForURL` requests image data insertion into store

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -16,14 +16,16 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
 
     private(set) var receivedMessages = [Message]()
     private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
-
-    func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
-        receivedMessages.append(.insert(data: data, for: url))
-    }
+    private var insertionCompletions = [(FeedImageDataStore.InsertionResult) -> Void]()
 
     func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
         receivedMessages.append(.retrieve(dataFor: url))
         retrievalCompletions.append(completion)
+    }
+    
+    func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        receivedMessages.append(.insert(data: data, for: url))
+        insertionCompletions.append(completion)
     }
 
     func completeRetrieval(with error: Error, at index: Int = 0) {
@@ -32,5 +34,9 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
 
     func completeRetrieval(with data: Data?, at index: Int = 0) {
         retrievalCompletions[index](.success(data))
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](.failure(error))
     }
 }
