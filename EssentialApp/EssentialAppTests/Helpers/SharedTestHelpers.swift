1393126b5fecebc3633a719dda9e1b886ec27d01
Yuriy Yasinskyy <yura.yasinskyy@gmail.com>
2025-04-26 14:18:56 +0200

    Implement `FeedImageDataLoaderWithFallbackComposite` to compose remote and local `FeedImageDataLoader` instances

---
+++ EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,20 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialAppTests
+//
+//  Created by Work on 26.04.2025.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+func anyData() -> Data {
+    return Data("any data".utf8)
+}
