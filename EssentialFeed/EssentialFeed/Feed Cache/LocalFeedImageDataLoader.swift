6b2e2d224fbf73b82af7ffcf29ae803c44d940bc
Yuriy Yasinskyy <yura.yasinskyy@gmail.com>
2025-04-27 14:50:42 +0200

    Make the `LocalFeedImageDataLoader` conform to `FeedImageDataCache` protocol (so it can be composed)

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -15,8 +15,8 @@ public final class LocalFeedImageDataLoader {
     }
 }
 
-extension LocalFeedImageDataLoader {
-    public typealias SaveResult = Result<Void, Error>
+extension LocalFeedImageDataLoader: FeedImageDataCache {
+    public typealias SaveResult = FeedImageDataCache.SaveResult
     
     public enum SaveError: Error {
         case failed
