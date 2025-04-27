111c221a5ef2d26f44a90abb4e74a7be7c0f0c1d
Yuriy Yasinskyy <yura.yasinskyy@gmail.com>
2025-04-27 13:38:03 +0200

    `FeedLoaderCacheDecorator.load` delivers decoratee loader result (either success or failure)

--- EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
@@ -7,5 +7,5 @@ import Foundation
 public protocol FeedLoader {
     typealias Result = Swift.Result<[FeedImage], Error>
     
-	func load(completion: @escaping (Result) -> Void)
+    func load(completion: @escaping (FeedLoader.Result) -> Void)
 }
