02b20c63c450e6b61bf2393d62e3d4f42a24848a
Yuriy Yasinskyy <yura.yasinskyy@gmail.com>
2025-04-28 22:24:21 +0200

    Check number of cells before fetching cell at index to avoid out of bounds exception

--- EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -87,6 +87,8 @@ extension FeedViewController {
     }
     
     func feedImageView(at row: Int) -> UITableViewCell? {
+        guard numberOfRenderedFeedImageViews() > row else { return nil }
+        
         let index = IndexPath(row: row, section: feedImagesSection)
         let ds = tableView.dataSource
         return ds?.tableView(tableView, cellForRowAt: index)
