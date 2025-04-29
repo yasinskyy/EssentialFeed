fec984dbef72577757185a0823186e43fb406cc3
Yuriy Yasinskyy <yura.yasinskyy@gmail.com>
2025-04-29 16:03:29 +0200

    Revert tolerance to `0.00001`

--- EssentialFeed/EssentialFeediOSTests/Helpers/XCTestCase+Snapshot.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/XCTestCase+Snapshot.swift
@@ -21,7 +21,7 @@ extension XCTestCase {
             return
         }
 
-        if !match(snapshotData, storedSnapshotData, tolerance: 0.9) {
+        if !match(snapshotData, storedSnapshotData, tolerance: 0.00001) {
             let temporarySnapshotURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
                 .appendingPathComponent(snapshotURL.lastPathComponent)
 
